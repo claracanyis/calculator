88c81c2bf9c2617366b2e0d848b82290a55f7114
claracanyis <claraccc9@gmail.com>
2026-05-10 20:43:11 +0200

    Create calculator operation function

--- calc.js
+++ calc.js
@@ -14,3 +14,21 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+// Calculator operation
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            add(num1,num2);
+            break;
+        case '-':
+            substract(num1,num2);
+            break;
+        case '*':
+            multiply(num1,num2);
+            break;
+        case '/':
+            divide(num1,num2);
+            break;
+    }
+}
