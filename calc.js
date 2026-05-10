fff1f02c1048d2ce0c494058df1ee615984287bd
claracanyis <claraccc9@gmail.com>
2026-05-10 23:03:45 +0200

    Make the calculator look cool

--- calc.js
+++ calc.js
@@ -19,16 +19,13 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
     switch(operator) {
         case '+':
-            add(num1,num2);
-            break;
+            return add(num1,num2);
         case '-':
-            substract(num1,num2);
-            break;
-        case '*':
-            multiply(num1,num2);
-            break;
+            return substract(num1,num2);
+        case 'x':
+            return multiply(num1,num2);
         case '/':
-            divide(num1,num2);
-            break;
+            return divide(num1,num2);
     }
 }
+
