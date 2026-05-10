2957e9b14cfbcdba0db4391f3375372f88004d17
claracanyis <claraccc9@gmail.com>
2026-05-10 23:04:50 +0200

    Get calculator to work when used correctly

--- calc.js
+++ calc.js
@@ -29,3 +29,59 @@ function operate(operator, num1, num2) {
     }
 }
 
+// Global variables
+let display = document.querySelector("#display");
+let buttons = document.querySelector("#buttons");
+let number1;
+let number2;
+let operator;
+let calcStep = 'start';
+
+buttons.addEventListener('click', function (event) {
+    console.log(event.target.className);
+    switch(event.target.className) {
+        case 'number':
+            if (calcStep == 'num1' || calcStep == 'num2') {
+                display.textContent += event.target.textContent;
+            } else if (calcStep == 'start') {
+                display.textContent = event.target.textContent;
+                calcStep = 'num1'
+            } else{
+                display.textContent = event.target.textContent;
+                calcStep = 'num2';
+            }
+            break;
+        case 'operator':
+            if (calcStep == 'num1' || calcStep == 'op') {
+                number1 = parseInt(display.textContent);
+                operator = event.target.textContent;
+                console.log(number1);
+                console.log(operator);
+            } else {
+                number2 = parseInt(display.textContent);
+                number1 = operate(operator, number1, number2);
+                display.textContent = number1;
+                operator = event.target.textContent;
+            }
+            calcStep = 'op';
+            break;
+        case 'equal':
+            if (calcStep == 'num2') {
+                number2 = parseInt(display.textContent);
+                display.textContent = operate(operator,number1,number2);
+                operator = '';
+                number1 = parseInt(display.textContent);
+                number2 = null;
+                calcStep = 'start';
+            }
+            break;
+        case 'clear':
+            calcStep = 'start';
+            number1 = 0;
+            number2 = 0;
+            operator = '';
+            display.textContent = '';
+    }
+    console.log(calcStep);
+    //console.log('num1: ' + number1 + '; op: ' + operator + '; num2: ' + number2);
+})
