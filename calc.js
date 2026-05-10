61dcad9b686f57b2a945fb97511caade068aa953
claracanyis <claraccc9@gmail.com>
2026-05-10 20:37:46 +0200

    Create basic math operator functions

---
+++ calc.js
@@ -0,0 +1,16 @@
+// Basic mathematic operators
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function substract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
